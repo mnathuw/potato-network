8b060369768e708af1588255bae911969df89bda
unknown <mnathu98@gmail.com>
2022-09-27 21:42:56 -0500

    make it better

--- client/src/components/LoadMoreBtn.js
+++ client/src/components/LoadMoreBtn.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const LoadMoreBtn = ({result, page, load, handleLoadMore}) => {
-    return (
-        <>
-            {
-                result < 9 * (page - 1) ? '' : 
+const LoadMoreBtn = ({ result, page, load, handleLoadMore }) => {
+  return (
+    <>
+      {result < 9 * (page - 1)
+        ? ""
+        : !load && (
+            <button
+              className="btn btn-dark mx-auto d-block"
+              onClick={handleLoadMore}
+            >
+              Load more
+            </button>
+          )}
+    </>
+  );
+};
 
-                !load && <button className="btn btn-dark mx-auto d-block"
-                onClick={handleLoadMore}>
-                    Load more
-                </button>
-            }
-            
-        </>
-    )
-}
-
-export default LoadMoreBtn
+export default LoadMoreBtn;
